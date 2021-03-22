a2f08600b6bc8e816c27d8994d9d247d374ddcc3
SergAlKoba <Serg.Al.Koba@gmail.com>
2021-03-22 09:45:55 +0200

    section reviews

--- assets/js/home.js
+++ assets/js/home.js
@@ -12,10 +12,29 @@ $(function(){
     });
 
     $('.portfolio .filter li').on('click', function(){
-        $('.filter li').removeClass('active');
+        $('.portfolio li').removeClass('active');
         $(this).addClass('active');
     });
 
     jcf.replaceAll();
+
+    var customersSlider = $('.customers-slider');
+
+    customersSlider.on("init", function(event, slick){
+        $(".customers_count").html(parseInt(slick.currentSlide + 1) + ' / <span>' + slick.slideCount + '</span>');
+    });
+
+    customersSlider.on("afterChange", function(event, slick, currentSlide){
+        $(".customers_count").html(parseInt(slick.currentSlide + 1) + ' / <span>' + slick.slideCount + '</span>');
+    });
+
+    customersSlider.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nextArrow: '<button class="slick-arrow next"><img src="../img/arrow-slider.svg"></button>',
+        prevArrow: '<button class="slick-arrow prev"><img src="../img/arrow-slider.svg"></button>',
+    });
+
+    $("#my-accordion").accordionjs();
 
 });
