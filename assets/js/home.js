e2e315f557c147583bb373208fc2588c9a84b38e
SergAlKoba <Serg.Al.Koba@gmail.com>
2021-03-23 13:49:18 +0200

    section portfolio

--- assets/js/home.js
+++ assets/js/home.js
@@ -37,4 +37,23 @@ $(function(){
 
     $("#my-accordion").accordionjs();
 
+    var customersSlider = $('.portfolio-slider');
+
+    customersSlider.on("init", function(event, slick){
+        $(".portfolio_count").html(parseInt(slick.currentSlide + 1) + ' / <span>' + slick.slideCount + '</span>');
+    });
+
+    customersSlider.on("afterChange", function(event, slick, currentSlide){
+        $(".portfolio_count").html(parseInt(slick.currentSlide + 1) + ' / <span>' + slick.slideCount + '</span>');
+    });
+
+    customersSlider.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        nextArrow: '<button class="slick-arrow next"><img src="../img/arrow-slider.svg"></button>',
+        prevArrow: '<button class="slick-arrow prev"><img src="../img/arrow-slider.svg"></button>',
+    });
+
+
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 $(function(){
 
-    
+    $(".top-link").on("click", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 800);
+    });
 
 });
