464f95a2c01b1a74bb7bcadcfaf0a0520d4be6cb
Anton Milasenko <48668353+Miant1@users.noreply.github.com>
2021-03-29 11:02:42 +0300

    last changes from 29.03

--- assets/js/home.js
+++ assets/js/home.js
@@ -55,5 +55,29 @@ $(function(){
         prevArrow: '<button class="slick-arrow prev"><img src="../img/arrow-slider.svg"></button>',
     });
 
+    //Reviews
+    let reviewsSlider = $('.reviews-slider');
 
+    reviewsSlider.on("init", function(event, slick){
+        $(".reviews_count").html(parseInt(slick.currentSlide + 1) + ' / <span>' + slick.slideCount + '</span>');
+    });
+
+    reviewsSlider.on("afterChange", function(event, slick, currentSlide){
+        $(".reviews_count").html(parseInt(slick.currentSlide + 1) + ' / <span>' + slick.slideCount + '</span>');
+    });
+
+    $(window).on('load resize', function(){
+        if($(window).width() < 1024){
+            reviewsSlider.slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                adaptiveHeight: true,
+                nextArrow: '<button class="slick-arrow next"><img src="../img/arrow-slider.svg"></button>',
+                prevArrow: '<button class="slick-arrow prev"><img src="../img/arrow-slider.svg"></button>',
+            });
+        }else{
+            reviewsSlider.unslick();
+        }
+    });
+    
 });
