a43147ff684d9a21bae51a20b6fe5b2c1a30e45c
Anton Milasenko <48668353+Miant1@users.noreply.github.com>
2021-04-22 13:38:28 +0300

    last changes from 22.04

---
+++ assets/js/advertisers.js
@@ -0,0 +1,29 @@
+$(function(){
+
+    jcf.replaceAll();
+    $("#my-accordion").accordionjs();
+
+    $('.portfolio .filter li').on('click', function(){
+        $('.portfolio li').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    // Portfolio slider
+    var portfolioSlider = $('.portfolio-slider');
+
+    portfolioSlider.on("init", function(event, slick){
+        $(".portfolio_count").html(parseInt(slick.currentSlide + 1) + ' / <span>' + slick.slideCount + '</span>');
+    });
+
+    portfolioSlider.on("afterChange", function(event, slick, currentSlide){
+        $(".portfolio_count").html(parseInt(slick.currentSlide + 1) + ' / <span>' + slick.slideCount + '</span>');
+    });
+
+    portfolioSlider.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        nextArrow: '<button class="slick-arrow next"><img src="img/arrow-slider.svg"></button>',
+        prevArrow: '<button class="slick-arrow prev"><img src="img/arrow-slider.svg"></button>',
+    });
+});
