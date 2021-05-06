6cdc67042df477c9569e4a7be2a2323247c864f6
Anton Milasenko <48668353+Miant1@users.noreply.github.com>
2021-05-06 09:59:31 +0300

    last changes from 6.05

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,23 @@ $(function(){
         $('body,html').animate({scrollTop: top}, 800);
     });
 
+    // Header
+
+    $('.open-menu').on('click', function() {
+        $('.header').find('nav').toggle();
+    });
+
+    // POPUPS
+
+    if($('.project-popup').hasClass('show') || $('.thanks-project-popup').hasClass('show') || $('.error-project-popup').hasClass('show') || $('.vacancy-popup').hasClass('show') || $('.reviews-popup').hasClass('show') || $('.thanks-reviews-popup').hasClass('show') || $('.error-reviews-popup').hasClass('show')){
+
+        $('body').css('overflow', 'hidden');
+    }
+    $('.close-popup').on('click', () => {
+        $('.project-popup, .thanks-project-popup, .error-project-popup, .vacancy-popup, .reviews-popup, .thanks-reviews-popup, .error-reviews-popup').removeClass('show');
+        
+        $('.project-popup, .thanks-project-popup, .error-project-popup, .vacancy-popup, .reviews-popup, .thanks-reviews-popup, .error-reviews-popup').addClass('hide');
+        $('body').css('overflow', 'auto');
+    });
+
 });
